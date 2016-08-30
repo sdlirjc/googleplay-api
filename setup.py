d3ff5efde8c994405aedb910a8bd878800c4fb25
Jinjian Zhai <zhaijinjian@gmail.com>
2016-08-30 11:04:05 -0700

    Build the googleplay-api into a pip package

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='googleplay-api',
+      version='1.1',
+      description='Google Play API',
+      author='Jinjian Zhai',
+      author_email='zhaijinjian@gmail.com',
+      url='cs.stanford.edu/~jameszjj',
+      packages=[''],
+     )
+
