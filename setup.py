15e7df264144830666749344a96fd1089b89931c
Jinjian Zhai <zhaijinjian@gmail.com>
2016-08-30 11:11:34 -0700

    Remove invalid char - from package name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from distutils.core import setup
 
-setup(name='googleplay-api',
+setup(name='googleplayapi',
       version='1.1',
       description='Google Play API',
       author='Jinjian Zhai',
